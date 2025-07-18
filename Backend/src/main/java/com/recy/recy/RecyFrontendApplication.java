ba505efae2a54ac68a956cf5a6d72e833a22fe3d
Akash Adak <akashadak00023@gmail.com>
2025-07-18 20:29:16 +0530

    first

---
+++ Backend/src/main/java/com/recy/recy/RecyFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.recy.recy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyFrontendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/recy/recy/RecyFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recy.recy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
